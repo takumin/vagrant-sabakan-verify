16eab591adf25296ecbf06970d86853988f3fbb3
takumin <takumiiinn@gmail.com>
2019-04-01 21:14:58 +0900

    [helpers/keeper] Parse proxy environment variables

--- helpers/keeper.rb
+++ helpers/keeper.rb
@@ -21,3 +21,41 @@ when 'ubuntu'
   end
   node['platform_family'] ||= 'debian'
 end
+
+proxy_regex = /^(socks|https?):\/\/(?:([0-9a-zA-Z-_\.]+):([0-9a-zA-Z-_\.]+)@)?([0-9a-zA-Z-_\.]+)(?::([0-9]+))?.*/
+
+if ENV['HTTP_PROXY'] then
+  node[:proxy][:proto] ||= ENV['HTTP_PROXY'].gsub(proxy_regex, '\1')
+  node[:proxy][:user]  ||= ENV['HTTP_PROXY'].gsub(proxy_regex, '\2')
+  node[:proxy][:pass]  ||= ENV['HTTP_PROXY'].gsub(proxy_regex, '\3')
+  node[:proxy][:host]  ||= ENV['HTTP_PROXY'].gsub(proxy_regex, '\4')
+  node[:proxy][:port]  ||= ENV['HTTP_PROXY'].gsub(proxy_regex, '\5').to_i
+  node[:proxy][:uri]   ||= ENV['HTTP_PROXY']
+elsif ENV['http_proxy'] then
+  node[:proxy][:proto] ||= ENV['http_proxy'].gsub(proxy_regex, '\1')
+  node[:proxy][:user]  ||= ENV['http_proxy'].gsub(proxy_regex, '\2')
+  node[:proxy][:pass]  ||= ENV['http_proxy'].gsub(proxy_regex, '\3')
+  node[:proxy][:host]  ||= ENV['http_proxy'].gsub(proxy_regex, '\4')
+  node[:proxy][:port]  ||= ENV['http_proxy'].gsub(proxy_regex, '\5').to_i
+  node[:proxy][:uri]   ||= ENV['http_proxy']
+elsif ENV['HTTPS_PROXY'] then
+  node[:proxy][:proto] ||= ENV['HTTPS_PROXY'].gsub(proxy_regex, '\1')
+  node[:proxy][:user]  ||= ENV['HTTPS_PROXY'].gsub(proxy_regex, '\2')
+  node[:proxy][:pass]  ||= ENV['HTTPS_PROXY'].gsub(proxy_regex, '\3')
+  node[:proxy][:host]  ||= ENV['HTTPS_PROXY'].gsub(proxy_regex, '\4')
+  node[:proxy][:port]  ||= ENV['HTTPS_PROXY'].gsub(proxy_regex, '\5').to_i
+  node[:proxy][:uri]   ||= ENV['HTTPS_PROXY']
+elsif ENV['https_proxy'] then
+  node[:proxy][:proto] ||= ENV['https_proxy'].gsub(proxy_regex, '\1')
+  node[:proxy][:user]  ||= ENV['https_proxy'].gsub(proxy_regex, '\2')
+  node[:proxy][:pass]  ||= ENV['https_proxy'].gsub(proxy_regex, '\3')
+  node[:proxy][:host]  ||= ENV['https_proxy'].gsub(proxy_regex, '\4')
+  node[:proxy][:port]  ||= ENV['https_proxy'].gsub(proxy_regex, '\5').to_i
+  node[:proxy][:uri]   ||= ENV['https_proxy']
+end
+
+if ENV['NO_PROXY'] then
+  node[:proxy][:bypass] ||= ENV['NO_PROXY'].split(',')
+elsif ENV['no_proxy'] then
+  node[:proxy][:bypass] ||= ENV['no_proxy'].split(',')
+end
